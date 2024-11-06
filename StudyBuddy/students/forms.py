372f009bd2af7ba3c91b64335bb82594c92d1bf0
timdecena <anthonycdecena@gmail.com>
2024-11-06 21:01:58 +0800

    Added more attributes

--- StudyBuddy/students/forms.py
+++ StudyBuddy/students/forms.py
@@ -1,8 +1,9 @@
 # students/forms.py
+
 from django import forms
 from .models import Student
 
 class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
-        fields = ['student_id', 'year']
+        fields = ['student_id', 'fullname', 'year', 'course', 'section']
--- StudyBuddy/students/models.py
+++ StudyBuddy/students/models.py
@@ -1,8 +1,13 @@
+# students/models.py
+
 from django.db import models
 
 class Student(models.Model):
     student_id = models.CharField(max_length=20, unique=True)  # Unique identifier for each student
+    fullname = models.CharField(max_length=100, default="Default Full Name") # Full name of the student
     year = models.IntegerField()  # Represents the year level of the student
+    course = models.CharField(max_length=50, default="Default Course")  # Course enrolled
+    section = models.CharField(max_length=10, default="Default Section")  # Section of the student
 
     def __str__(self):
-        return self.student_id
+        return f"{self.student_id} - {self.fullname}"
