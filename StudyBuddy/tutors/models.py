9415336c093848d5f0aa1e91f47b415c0e0653a8
tyronebeldad123 <tyronebeldad123@gmail.com>
2024-11-19 13:07:18 +0800

    Pass and User for Tutors
    - Added password and username variables for Tutors

--- StudyBuddy/tutors/models.py
+++ StudyBuddy/tutors/models.py
@@ -5,6 +5,8 @@ class Tutor(models.Model):
     last_name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
     subject = models.CharField(max_length=100)
+    username = models.CharField(max_length=50, unique=True)  # Added username field
+    password = models.CharField(max_length=128)  # Added password field
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
