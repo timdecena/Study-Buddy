8faea51dc511ef38bff97aa3013768c7e51dd9a0
B4rryAli <christianbarryalico@gmail.com>
2024-12-08 15:38:43 +0800

    Transaction css

--- StudyBuddy/transaction/forms.py
+++ StudyBuddy/transaction/forms.py
@@ -1,23 +1,30 @@
 from django import forms
 from .models import Transaction
-
 from students.models import Student  # Import the Student model
 from tutors.models import Tutor
 from friend_requests.models import FriendRequest
 
-# forms.py
 class TransactionForm(forms.ModelForm):
+    PAYMENT_METHOD_CHOICES = [
+        ('gcash', 'GCash'),
+        ('paymaya', 'PayMaya'),
+        ('credit_card', 'Credit Card'),
+        ('bank_transfer', 'Bank Transfer'),
+        ('paypal', 'PayPal'),
+        ('cash', 'Cash'),
+    ]
+
+    payment_method = forms.ChoiceField(
+        choices=PAYMENT_METHOD_CHOICES, 
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+
     class Meta:
         model = Transaction
         fields = ['payment_method', 'payment_amount', 'tutor']
         widgets = {
-            'payment_amount': forms.NumberInput(attrs={'step': '0.01'}),
+            'payment_amount': forms.NumberInput(attrs={'step': '0.01', 'class': 'form-control'}),
         }
-
-class TransactionForm(forms.ModelForm):
-    class Meta:
-        model = Transaction
-        fields = ['payment_method', 'payment_amount', 'tutor']
 
     def __init__(self, *args, **kwargs):
         student = kwargs.pop('student', None)
